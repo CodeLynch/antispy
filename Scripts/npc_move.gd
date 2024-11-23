64d7e4ddadea43e8df50228acaf6a03755eec4d1
CodeLynch <torpedonialingnena@gmail.com>
2024-11-23 12:03:36 +0800

    prototype - walk animations added

--- Scripts/npc_move.gd
+++ Scripts/npc_move.gd
@@ -6,6 +6,8 @@ extends CharacterBody2D
 @onready var detect_radius: Area2D = $DetectRadius
 @onready var clickable_area: Area2D = $ClickableArea
 @onready var npc_char: NPCChar = $NPCChar
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 
 signal npc_clicked(id:int, name:String, role_int: int, role_tex: String, is_spy:bool)
 signal npc_died(is_spy:bool)
@@ -16,6 +18,7 @@ var movement_target_position: Vector2i = Vector2i(0,100)
 var rng = RandomNumberGenerator.new()
 var cancel_move: bool = false
 var is_player_near: bool = false
+var last_dir = 0
 var escape: bool = false
 var orig_movement_speed
 var orig_movement_len
@@ -34,8 +37,8 @@ func set_params(id: int, name: String, role_no:int, is_anxious: bool, is_sus: bo
 	$NPCChar.is_spy = is_spy 
 	
 
-func _init() -> void:
-	movement_target_position = global_position + get_random_dir() * movement_len
+#func _init() -> void:
+#	movement_target_position = global_position + get_random_dir() * movement_len
 	
 	
 func _ready():
@@ -57,39 +60,41 @@ func _process(delta):
 
 func _physics_process(_delta):
 	if $NPCChar.near_player:
+		change_anim_from_vec(Vector2i(0,0))
 		return
 		
-	if $NPCChar.is_anxious :
-		if $NPCChar.near_player and !escape:
-			cancel_move = true
-			escape = true
-		elif !$NPCChar.near_player:
-			cancel_move = false
-			escape = false
-			
+	#if $NPCChar.is_anxious :
+		#if $NPCChar.near_player and !escape:
+			#cancel_move = true
+			#escape = true
+		#elif !$NPCChar.near_player:
+			#cancel_move = false
+			#escape = false
+			#
 	if navigation_agent.is_navigation_finished() or cancel_move:
-		if escape:
-			movement_speed *= 1.5
-			movement_target_position = (global_position + $NPCChar.esc_vector * movement_len)
+		#if escape:
+			#movement_speed *= 1.5
+			#movement_target_position = (global_position + $NPCChar.esc_vector * movement_len)
+			#set_movement_target(movement_target_position)
+			#cancel_move = false
+#else:
+		movement_speed = orig_movement_speed
+		handle_stop()
+		if (!is_paused):
+			var mov_vec = get_random_dir()
+			movement_target_position = global_position + mov_vec * movement_len
 			set_movement_target(movement_target_position)
 			cancel_move = false
-		else:
-			movement_speed = orig_movement_speed
-			handle_stop()
-			if (!is_paused):
-				movement_target_position = global_position + get_random_dir() * movement_len
-				set_movement_target(movement_target_position)
-				cancel_move = false
-				
+					
 		return
 
 	var current_agent_position: Vector2 = global_position
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
 
 	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+	change_anim_from_vec(velocity)
 	move_and_slide()
 	var col_count = get_slide_collision_count()
-	print(col_count)
 	if col_count > 0:
 		cancel_move = true
 
@@ -102,6 +107,30 @@ func get_random_dir():
 	var rand_num = rng.randi_range(0, 3)
 	return possible_vectors[rand_num]
 
+
+func change_anim_from_vec(vec: Vector2i) -> void:
+	if vec.x != 0:
+		animated_sprite_2d.animation = "walk_s"
+		last_dir = 2
+		if vec.x > 0:
+			animated_sprite_2d.flip_h = false
+		else:
+			animated_sprite_2d.flip_h = true
+	elif vec.y != 0:
+		if vec.y > 0:
+			animated_sprite_2d.animation = "walk_f"
+			last_dir = 0
+		else:
+			animated_sprite_2d.animation = "walk_b"
+			last_dir = 1
+	else:
+		if last_dir == 0:
+			animated_sprite_2d.animation = "idle"
+		elif last_dir == 1:
+			animated_sprite_2d.animation = "back"
+		else:
+			animated_sprite_2d.animation = "side"
+	
 func actor_setup():
 	await get_tree().physics_frame
 	set_movement_target(movement_target_position)
@@ -116,6 +145,8 @@ func handle_stop():
 	if(!is_paused):
 		if(rng.randi_range(1, 100) <= pause_chance):
 			is_paused = true
+			change_anim_from_vec(Vector2i(0,0))
+			
 
 
 func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
--- Scripts/player_script.gd
+++ Scripts/player_script.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 const speed = 150
 var direction = Vector3()
 var role_arr:Array = ["field_agent", "analyst", "rd_eng", "janitor"]
+var last_dir = 0
 @onready var sprite = $AnimatedSprite2D
 @onready var spy_detect: Area2D = $SpyDetect
 @export var can_move = true
@@ -18,6 +19,26 @@ func _physics_process(_delta):
 
 func get_input():
 	var dir = Input.get_vector("move_left", "move_right", "move_up", "move_down");
+	if dir.x != 0:
+		if dir.x > 0:
+			sprite.flip_h = false
+		else:
+			sprite.flip_h = true
+		sprite.animation = "walk_s"
+		last_dir = 2
+	elif dir.y > 0:
+		sprite.animation = "walk_f"
+		last_dir = 0
+	elif dir.y < 0:
+		sprite.animation = "walk_b"
+		last_dir = 1
+	else:
+		if last_dir == 1:
+			sprite.animation = "back"
+		elif last_dir == 2:
+			sprite.animation = "side"
+		else:
+			sprite.animation = "idle"	
 	velocity = dir.normalized() * speed
 	direction = velocity
 
